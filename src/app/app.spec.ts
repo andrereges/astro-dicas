e54f5be3b1b9c3e2e37e05fefc08a1c4777397ab
Andre Reges <dreeh.silva@outlook.com>
2026-01-11 20:42:19 -0300

    fix: Ajustado logo e webmanifest

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,25 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { TipListPage } from '../app/pages/tip/list/tip-list.page';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [TipListPage],
       providers: [provideZonelessChangeDetection()]
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(TipListPage);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(TipListPage);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, astro-select-dicas');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Indicadica');
   });
 });
